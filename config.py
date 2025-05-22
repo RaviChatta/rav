7d7f58e298c4e5f067aa0ecaab41da202f0ffbd9
erennew <erenyeagermikasa84@gmail.com>
2025-05-23 00:42:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -58,6 +58,6 @@ class Settings():
     
     # URL par défaut pour l'image de démarrage ou de log
     IMAGES = os.getenv("IMAGES", "https://graph.org/file/7c1856ae9ba0a15065ade-abf2c0b5a93356da7b.jpg")
-    ANIMATIONS = os.getenv("ANIMATIONS", "").strip()
+    ANIMATIONS = os.getenv("ANIMATIONS", "CgACAgUAAxkBAAIqu2gvdr3Ig6KwAjQdM9woyO2OApJsAAI9JQACQFB4VZDEV3IIg0VcHgQ").strip()
 # Crée une instance de Settings pour un accès global
 settings = Settings()
