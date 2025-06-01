d145830fd424600c7abdda4871c8209c63db4292
erennew <erenyeagermikasa84@gmail.com>
2025-06-01 06:46:03 +0530

    Update compat.py

--- compat.py
+++ compat.py
@@ -1,10 +1,20 @@
-# compat.py
+# compat.py - Universal compatibility layer
 import sys
 import mimetypes
+from typing import Any
 
-class ImghdrCompat:
-    @staticmethod
-    def what(file, h=None):
-        return mimetypes.guess_type(file)[0]
+class _Compat:
+    def __getattr__(self, name: str) -> Any:
+        if name == 'imghdr':
+            return self._imghdr_compat()
+        raise AttributeError(name)
 
-sys.modules['imghdr'] = ImghdrCompat()
+    def _imghdr_compat(self):
+        class ImghdrCompat:
+            @staticmethod
+            def what(file, h=None):
+                mime = mimetypes.guess_type(file)[0]
+                return mime.split('/')[-1] if mime else None
+        return ImghdrCompat()
+
+sys.modules[__name__] = _Compat()
